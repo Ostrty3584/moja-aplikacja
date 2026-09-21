4254cf6f0d27535e926fa9cec16acb1b58b8390c
ostry3584 <kacperostrowski3584@gmail.com>
2026-09-21 10:33:25 +0200

    Dodano pierwsze komponenty React

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Moja pierwsza aplikacja</h1>
+            <p>Uczę się budować aplikacje</p>
+        </header>
+    );
+}
---
+++ components/Services.tsx
@@ -0,0 +1,10 @@
+export default function Services() {
+    return (
+        <section>
+            <h2> Moje usługi</h2>
+            <p>Tworzenie stron</p>
+            <p>Kalkulatory internetowe</p>
+            <p>Proste aplikacje</p>
+        </section>
+    )
+}
